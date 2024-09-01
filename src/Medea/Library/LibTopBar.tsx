c14bc92dff55d1fcd43790d88681d35020673d3a
Stephen Romero <stephen.romero93@gmail.com>
2024-09-01 08:36:23 -0500

    Added event hander props for add url form

--- src/Medea/Library/LibTopBar.tsx
+++ src/Medea/Library/LibTopBar.tsx
@@ -1,35 +1,47 @@
+import { useState } from "react";
+import AddVideo from "../../AddVideo";
 
 
-const clickedButton =  (e: React.SyntheticEvent): void => {
-    let target = e.target as HTMLInputElement;
-    console.log(target);
-}
+const LibTopBar = () => {
+  const [openForm, setOpenForm] = useState(false);
 
-const LibTopBar = () => (
-  <div
-    style={{
-      height: "20%",
-      width: "100%",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      borderBottom: "1px solid grey",
-    }}
-  >
-    <h3 style={{ color: "white" }}>Recently Added</h3>
-    <button
-      style={{
-        color: "white",
-        backgroundColor: "#1F1F1F",
-        height: "40px",
-        borderRadius: "5px",
-      }}
-      onClick={clickedButton}
-    >
-      Add New Video
-    </button>
-  </div>
-);
+  const clickedButton = (e: React.SyntheticEvent): void => {
+    setOpenForm(!openForm);
+  };
+
+  const setOpen = (openModal: boolean) => {
+    setOpenForm(openModal);
+    return true
+  }
+  return (
+    <>
+      <div
+        style={{
+          height: "20%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <h3 style={{ color: "white" }}>Recently Added</h3>
+        <button
+          style={{
+            color: "white",
+            backgroundColor: "#1F1F1F",
+            height: "40px",
+            borderRadius: "5px",
+          }}
+          onClick={clickedButton}
+        >
+          Add New Video
+        </button>
+      </div>
+      {openForm && <AddVideo setOpen={(open) => setOpen(open)}/>}
+    </>
+  );
+};
 
 export default LibTopBar;
